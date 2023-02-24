c51dc14154aa239be51a67932ef4f0294816a4b9
Patrick <73594427+Watersmash3@users.noreply.github.com>
2023-02-23 22:00:45 -0600

    Add Selection of Table Data
    This data will appear along the side of the table

--- src/employeeDatabase/employeeDatabaseController.java
+++ src/employeeDatabase/employeeDatabaseController.java
@@ -4,7 +4,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TablePosition;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
@@ -42,8 +44,15 @@ public class employeeDatabaseController implements Initializable {
     private TableColumn<GuardModel, Boolean> fullScreener;
     @FXML
     private TableColumn<GuardModel, String> location;
+    @FXML
+    private Label idLabel;
+    @FXML
+    private Label nameLabel;
 
     public void select() {
+        GuardModel guardModel = tableData.getSelectionModel().getSelectedItem();
+        idLabel.setText("ID: " + guardModel.getIdNumber());
+        nameLabel.setText("Name: " + guardModel.getName());
 
     }
 
